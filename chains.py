e51138d100bbf0ab6dffa343c4d3d147dac85617
AbdullahAlzariqi <abdallahtalat5597@gmail.com>
2024-12-03 15:16:00 +0300

    first commit

---
+++ chains.py
@@ -0,0 +1,6 @@
+from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    
+)
