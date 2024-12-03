8c36f96d491e1436b7e6556dab2e1fddafe3e333
AbdullahAlzariqi <abdallahtalat5597@gmail.com>
2024-12-04 00:21:33 +0300

    Finalized

--- chains.py
+++ chains.py
@@ -1,6 +1,32 @@
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 reflection_prompt = ChatPromptTemplate.from_messages(
-    
+
+    [
+        ("system",
+         "You are a viral twitter influencer grading a tweet. Gnerate critique and reccomendations for the user. Always provide detailed recommendations, including requests forlength, virality, style ,etc"
+         ),
+         MessagesPlaceholder(variable_name="messages")
+
+    ] 
 )
+
+
+generation_prompt= ChatPromptTemplate.from_messages(
+    [
+        ("system",
+         " You are a twitter techie influencer assistant tasked with writing excellent twitter posts. Generate the best twitter post possible for the user's request. If the user provides critique, respond with a revised version of the previous attempts"
+         ),
+         MessagesPlaceholder(variable_name="messages")
+
+    ]
+)
+
+llm = ChatGoogleGenerativeAI(    model="gemini-1.5-flash", api_key=os.getenv("GEMINI_API_KEY"))
+generation_chain = generation_prompt | llm
+reflection_chain = reflection_prompt | llm
--- main.py
+++ main.py
@@ -1,5 +1,36 @@
 from dotenv import load_dotenv
+from typing import List, Sequence
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.graph import END, MessageGraph
+from chains import reflection_chain, generation_chain
+
 load_dotenv()
 
-if __name__ == "__main__":
-    print("Hello World")
+REFLECT = "reflect"
+GENERATE = "generate"
+
+def generation_node(state: Sequence[BaseMessage]):
+    return generation_chain.invoke({"messages":state})
+
+def reflection_node(messages: Sequence[BaseMessage]) -> List[BaseMessage]:
+    res = reflection_chain.invoke({"messages":messages})
+    return [HumanMessage(content=res.content)]
+
+builder = MessageGraph()
+builder.add_node(GENERATE, generation_node)
+builder.add_node(REFLECT, reflection_node)
+builder.set_entry_point(GENERATE)
+
+def should_continue(state: List[BaseMessage]):
+    if len(state) > 6:
+        return END
+    return REFLECT
+
+builder.add_conditional_edges(GENERATE, should_continue)
+builder.add_edge(REFLECT,GENERATE)
+
+graph = builder.compile()
+print(graph.get_graph().draw_mermaid())
+
+if __name__ =="__main__":
+    print("hello world")
